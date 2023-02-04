f8e661bc1c5c940de219fb98921ae1de3268d5c9
fhub-1 <flutterhub1.21.0@gmail.com>
2023-02-04 19:42:59 +0300

    upadteding the read me file working next part

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { theme } from "../chakra/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-       <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
     </ChakraProvider>
   )
- 
+
 }
